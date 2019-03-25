4d545504c1ed183fd86a5de942c56cac80edc83a
Dhaval Heruwala <heruwala@gmail.com>
2019-03-25 15:38:08 -0400

    Update packagingDeployment.sh

--- cd/packagingDeployment.sh
+++ cd/packagingDeployment.sh
@@ -10,21 +10,21 @@
 #   Note: When you create a new Package, also create an extra version before using this script. This script is for updates to package.
 
 #   In Feature / Develop Branch
-#   To create subsequent versions & install from a developer workstation assuming alias for his/her Scratch org is MyScratchOrg, run
+#   1. To create subsequent versions & install from a developer workstation assuming alias for his/her Scratch org is MyScratchOrg, run
 #   cd/packagingDeployment.sh package MyScratchOrg
+#   2. Update PACKAGE_VERSION in this script as new stable version
 
-#   In Feature / Develop Branch
-#   To install latest version from a developer workstation only (already defined in script below) to the scratch org, run
+#   In Feature / Develop / Package Branch
+#   1. To install latest version from a developer workstation only (already defined in script below) to the scratch org, run
 #   cd/packagingDeployment.sh install MyScratchOrg
 
 #   In Package branch, 
-#   1. Update PACKAGE_VERSION in this script as new stable version
-#   2. Create a git tag with the version to trigger the UAT pipeline
+#   2. Create a git tag with the version and approve UAT deployment to trigger the UAT pipeline
 
 #   Before promoting to Production (promote package version)
 #   TODO: When running in Prod CD, promote the package version
 
-#   While in CD branches, install (QA, UAT, Prod environment)
+#   To install in Salesforce environment, using continuous deployment pipeline (QA, UAT, Prod environment)
 #   cd/packagingDeployment.sh
 #####################################################################################################################################
 
@@ -33,7 +33,7 @@
 # In CI-CD, the build folder becomes the working folder 
 BUILD_NAME="ECFMG.ES-Base-Objects - CI"
 PACKAGE_NAME="EzSpaceBaseObjects"
-PACKAGE_VERSION="EzSpaceBaseObjects@0.1.0-8"
+PACKAGE_VERSION="EzSpaceBaseObjects@0.1.0-9"
 
 # Default values
 ACTION=$1
